ff1dde566adcdc2396820414583ed9c00a3e09d6
Joydeep Ghosh <joydeep.ghosh@devexpresso.com>
2019-04-26 09:16:40 -0500

    updated ps files

--- azurecli/docker-build.ps1
+++ azurecli/docker-build.ps1
@@ -1,4 +1,4 @@
-#create images using docker compose
+echo "create images using docker compose"
 echo "Run Command: docker-compose -f docker-compose.yml -f docker-compose-production.yml up -d"
 echo "`n"
 Write-Output "--------------------------------------------------------------------"
@@ -6,7 +6,7 @@ docker-compose -f docker-compose.yml -f docker-compose-production.yml up -d
 Write-Output "--------------------------------------------------------------------"
 echo "`n"
 
-#verify docker images have been created
+echo "verify docker images have been created"
 echo "Run Command: docker images -a"
 echo "`n"
 Write-Output "--------------------------------------------------------------------"
@@ -14,7 +14,7 @@ docker images -a
 Write-Output "--------------------------------------------------------------------"
 echo "`n"
 
-#verify docker containers are running
+echo "verify docker containers are running"
 echo "Run Command: docker ps -a"
 echo "`n"
 Write-Output "--------------------------------------------------------------------"
